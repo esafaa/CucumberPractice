32d700f42f73511e12dba5cd15aab75211c31349
esafaa <132300437+esafaa@users.noreply.github.com>
2023-12-17 16:31:27 -0600

    Singleton design pattern for driver

--- src/test/java/stepDefinitions/LoginStep.java
+++ src/test/java/stepDefinitions/LoginStep.java
@@ -5,8 +5,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 import pages.LoginPage;
 import utilities.Driver;
 
